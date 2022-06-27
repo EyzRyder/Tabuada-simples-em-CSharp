55267223e42f854198422fa1ae2d181a7a6b88a3
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-06-26 23:15:21 -0300

    Add project files.

---
+++ App1Ds02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1Ds02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual tabuada que vc quer ver? ");
+            int tab = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{tab} x {i} = {tab*i}      {tab+1} x {i} = {(tab+1) * i}      {tab+2} x {i} = {(tab+2) * i}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
